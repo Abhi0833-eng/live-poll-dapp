ef806ba136d38f94eefb26db5a246d6bc01cbc3a
Abhishek Gupta <abhishekgupta0834@gmail.com>
2026-04-24 10:22:36 +0530

    feat: add smart contract source code in contract folder

--- contracts/poll/src/lib.rs
+++ contracts/poll/src/lib.rs
@@ -1,35 +1,21 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, contracttype, symbol_short, Env, Symbol, Map};
+use soroban_sdk::{contract, contractimpl, symbol_short, Env, Symbol};
 
-#[contracttype]
-pub enum DataKey {
-    Option(Symbol),
-    TotalVotes,
-}
+const YES: Symbol = symbol_short!("YES");
+const NO: Symbol = symbol_short!("NO");
+const MAYBE: Symbol = symbol_short!("MAYBE");
 
 #[contract]
 pub struct PollContract;
 
 #[contractimpl]
 impl PollContract {
-    pub fn vote(env: Env, option: Symbol) -> u32 {
-        let key = DataKey::Option(option.clone());
-        let count: u32 = env.storage().persistent().get(&key).unwrap_or(0);
-        let new_count = count + 1;
-        env.storage().persistent().set(&key, &new_count);
-
-        let total: u32 = env.storage().persistent().get(&DataKey::TotalVotes).unwrap_or(0);
-        env.storage().persistent().set(&DataKey::TotalVotes, &(total + 1));
-
-        new_count
+    pub fn vote(env: Env, option: Symbol) {
+        let count: u32 = env.storage().persistent().get(&option).unwrap_or(0);
+        env.storage().persistent().set(&option, &(count + 1));
     }
 
     pub fn get_votes(env: Env, option: Symbol) -> u32 {
-        let key = DataKey::Option(option);
-        env.storage().persistent().get(&key).unwrap_or(0)
-    }
-
-    pub fn get_total(env: Env) -> u32 {
-        env.storage().persistent().get(&DataKey::TotalVotes).unwrap_or(0)
+        env.storage().persistent().get(&option).unwrap_or(0)
     }
 }
